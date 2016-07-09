8e92db536f459984cfb9ac0f15bf8c7268c8bc90
dviramontes <dviramontes@gmail.com>
2016-07-08 21:45:07 -0600

    update wp-config witn env vars

--- web/wp-config.php
+++ web/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', getenv('DB_NAME'));
+define('DB_NAME', 'mmmanyfold-blog');
 
 /** MySQL database username */
 define('DB_USER', getenv('DB_USER'));
 
 /** MySQL database password */
-define('DB_PASSWORD', getenv('DB_PASS'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 /** MySQL hostname */
-define('DB_HOST', getenv('DB_HOST').':'.getenv('DB_PORT'));
+define('DB_HOST', getenv('DB_HOST'));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
